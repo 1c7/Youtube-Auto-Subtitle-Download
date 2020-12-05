02d366e2c7247705f51126e14109e2a44d879821
1c7 <guokrfans@gmail.com>
2020-12-06 00:56:09 +0800

    v27

--- Youtube-Subtitle-Downloader/Tampermonkey.js
+++ Youtube-Subtitle-Downloader/Tampermonkey.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name           Youtube Subtitle Downloader v26
+// @name           Youtube Subtitle Downloader v27
 // @include        https://*youtube.com/*
 // @author         Cheng Zheng
 // @copyright      2009 Tim Smart; 2011 gw111zz; 2014~2021 Cheng Zheng;
 // @license        GNU GPL v3.0 or later. http://www.gnu.org/copyleft/gpl.html
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @version        26
+// @version        27
 // @grant GM_xmlhttpRequest
 // @namespace https://greasyfork.org/users/5711
 // @description   Download Subtitles
@@ -341,9 +341,8 @@
           for (var i = 0, il = captions.length; i < il; i++) {
             caption = captions[i];
             let lang_code = caption.getAttribute('lang_code')
-            // let lang_original = caption.getAttribute('lang_original')
-            // let lang_translated = caption.getAttribute('lang_translated')
-            let lang_name = lang_code_to_local_name(lang_code)
+            let lang_translated = caption.getAttribute('lang_translated')
+            let lang_name = lang_code_to_local_name(lang_code, lang_translated)
             caption_info = {
               lang_code: lang_code,
               lang_name: lang_name,
@@ -606,20 +605,23 @@
   // Input a language code, output that language name in current locale
   // 如果当前语言是中文简体, Input: "de" Output: 德语
   // if current locale is English(US), Input: "de" Output: "Germany"
-  function lang_code_to_local_name(languageCode) {
-    var captionTracks = get_captionTracks()
-    for (var index in captionTracks) {
-      var caption = captionTracks[index];
-      if (caption.languageCode === languageCode) {
-        let simpleText = captionTracks[index].name.simpleText;
-        if (simpleText) {
-          return simpleText
-        } else {
-          return languageCode
+  function lang_code_to_local_name(languageCode, fallback_name) {
+    try {
+      var captionTracks = get_captionTracks()
+      for (var i in captionTracks) {
+        var caption = captionTracks[i];
+        if (caption.languageCode === languageCode) {
+          let simpleText = captionTracks[i].name.simpleText;
+          if (simpleText) {
+            return simpleText
+          } else {
+            return fallback_name
+          }
         }
       }
+    } catch (error) {
+      return fallback_name
     }
-    return languageCode
   }
 
   function get_title() {
